5ae2046d5df625dea85d86186912bdcb703f5c3e
ckenaley <cpkenaley@gmail.com>
2021-09-16 17:30:23 -0400

    proj1 feedback

---
+++ Project_1_Greg_comments.R
@@ -0,0 +1,66 @@
+### My comments beging with [CPK]
+
+library(ggplot2)
+library(tidyverse)
+
+# [CPK] Why set the working directory with something you're sharing with someone else? They'd have to have this same directory "scale" in "molecool", etc. Why not keep data in the repo/project directory (the default wd)?
+
+setwd("~/Desktop/molecool/scale")
+dat <- read.csv("scales.csv")
+
+# [CPK] from here on, lots of lines you don't nee  I know the dim of the data and what the first few lines look like, etc. Please stick to addressing the tasks.
+
+dim(dat)
+head(dat)
+class(dat$N)
+class(dat$quadrant)
+class(dat$species)
+class(dat$specimen)
+class(dat[,1])
+class(dat[,2])
+class(dat[,3])
+class(dat[,4])
+mean(dat$N)
+mean(dat$quadrant)
+sapply(dat,class)
+dat$species <- as.factor(dat$species)
+species <- levels(dat$species)
+species
+length(species)
+dat$species==species[1]
+dat$species[dat$species==species[1]]
+A.rup<-length(dat$species[dat$species==species[1]])
+L.gib<-length(dat$species[dat$species==species[2]])
+L.mac<-length(dat$species[dat$species==species[3]])
+M.sal<-length(dat$species[dat$species==species[4]])
+M.sax<-length(dat$species[dat$species==species[5]])
+P.fla<-length(dat$species[dat$species==species[6]])
+species.obs <- data.frame(sp=species,n=c(A.rup,L.gib,L.mac,M.sal,M.sax,P.fla))
+species.obs
+dat %>%
+  group_by(species) %>%
+  summarise(n=n())
+dat %>%
+    count(species,specimen) %>%
+    print() %>%
+    count(species,name="n.specimens")
+for(i in 1:10) print(i)
+for(i in species){
+    p <- dat %>%
+    filter(species==i)%>%
+  ggplot()+geom_boxplot(aes(x=quadrant,y=N))+ggtitle(i)
+  print(p)
+}
+pdf("species.quadrant.pdf")
+for(i in species){
+  p <- dat %>%
+    filter(species==i)%>%
+    ggplot()+geom_boxplot(aes(x=quadrant,y=N))+ggtitle(i)
+  print(p)
+}
+dev.off()
+list.files(pattern=".pdf")
+
+##edit? 2 3 4
+
+#Grade=10/10 ("Spot on" for both rubric items)
